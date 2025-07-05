e3c21916d399af8492e0064b8b8c34953d898c3a
Michael de Silva <michael@crustyengineer.com>
2025-07-05 18:08:53 +0530

    chore: add more notes on subtyping and variance

--- essential_rust/src/language_foundations/subtyping_variance.rs
+++ essential_rust/src/language_foundations/subtyping_variance.rs
@@ -5,11 +5,13 @@ mod illustrate_covariance {
     #[test]
     fn ex_1() {
         fn whatever<'short>() {
-            // By specifying PhantomData<fn() -> T> instead of PhantomData<T>, given F<T> we can
-            // say &'a T is covariant over 'a.
+            // Given F<T> we can say &'a T is covariant over 'a.
             //
             // The Rustonomicon specifies it is ok to treat &'a T as a subtype of &'b T
             // if 'a <: 'b (Sub <: Super).
+            //
+            // For the purpose of variance, PhantomData<T> does exactly the same thing as PhantomData<fn() -> T>
+            // They only differ in how they affect auto traits.
             struct Foo<T> {
                 inner: f64,
                 marker: PhantomData<fn() -> T>,
@@ -40,5 +42,73 @@ mod illustrate_covariance {
             }
             takes_foo::<'short>(foo);
         }
+    }
+
+    #[test]
+    fn ex_1_lifetimes() {
+        // 'longer: 'shorter specifies that longer completely outlives 'shorter
+        // 'longer is a subtype of 'shorter
+        // 'longer can be a 'shorter but a 'shorter cannot be a 'longer
+        fn symptomatic<'shorter, 'longer: 'shorter>(x: &'longer mut u32, y: &'shorter u32) {
+            // x = y; // will not type check
+        }
+    }
+
+    #[test]
+    fn shared_mut_invariance_over_t() {
+        struct Thing;
+
+        fn foo<'a, 'b>(x: &'a mut &'static Thing) {
+            // let _y: &'a mut &'b Thing = x;
+        }
+
+        let thing = &mut &Thing {};
+
+        // foo(thing);
+
+        // error: lifetime may not live long enough
+        //     --> src/language_foundations/subtyping_variance.rs:63:41
+        //     |
+        //     62 |         fn foo<'a, 'b>(x: &'a mut &'static Thing) {
+        //     |                    -- lifetime `'b` defined here
+        //     63 |             let _y: &'a mut &'b Thing = x;
+        //     |                                         ^ assignment requires that `'b` must outlive `'static`
+        //     |
+        //     = note: requirement occurs because of a mutable reference to `&Thing`
+        //     = note: mutable references are invariant over their type parameter
+        //     = help: see <https://doc.rust-lang.org/nomicon/subtyping.html> for more information about variance
+    }
+
+    // Ref: https://www.youtube.com/watch?v=iVYWDIW71jk
+    #[cfg(FALSE_CONST_DO_NOT_COMPILE)]
+    #[test]
+    fn jonhoo_shared_mut_invariance_over_t() {
+        fn foo<'a>(s: &mut &'a str, x: &'a str) {
+            *s = x;
+        }
+
+        // we cannot downgrade from 'static to 'a (where x is a static string reference)
+        let mut x: &'static str = "invariance is hard";
+
+        // this is a local thing
+        let z = String::new();
+
+        // &z: is a non-static string
+        foo(&mut x, &z);
+        // trying to reconcile, that these are the same - they are not:
+        //  // foo(&mut &'a         str, &'a str)
+        //  // foo(&mut &'static    str, &'a str)
+        drop(z);
+
+        // This is because mutable references are invariant over their type parameter
+        // &mut T is invariant over T
+
+        // If we did not have invariance for mutable references, you could downgrade a mutable reference
+        // to something that is a reference to something less useful, stick in something less useful but
+        // in the outer scope the thing you took a mutable reference TO still has the more useful type.
+        // And so you now have something that still has the more useful type and so now you have something
+        // that thinks it's a more useful type, but it's actually a less useful type.
+        //
+        // This is not okay because it doesn't have some of the properties that it's type indicates it has.
     }
 }
